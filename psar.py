6f3360aceddae4921e2726af87fa8456909fbcc9
Bill Perkins <bill.perkins@seacoastbank.com>
2020-05-19 10:36:19 -0400

    Add check for a datestamp Get system name for output Simplify More docs

--- psar.py
+++ psar.py
@@ -14,43 +14,48 @@ import subprocess
 # main()
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
+    """ Run /usr/bin/sar -P ALL along with an optional day # to get CPU usage
     """
-    """
-    flag = ['', '']
+
+    flag = ''
     minusage = 90.0
+    maxusage = 10.0
 
     if len(sys.argv) > 1:
         daystr = sys.argv[1]
         flag = ['-f', '/var/log/sa/sa' + sys.argv[1]]
 
     cmdline = ['/usr/bin/sar', '-P', 'ALL']
-    if flag[0] == '-f':
-        cmdline.append(flag[0])
-        cmdline.append(flag[1])
+    if len(flag) > 1:
+        cmdline += flag
 
-    work = subprocess.check_output(cmdline, stderr=subprocess.STDOUT)
-    work = work.decode('utf-8') # change from bytes to string
+    work = subprocess.check_output(cmdline, stderr=subprocess.STDOUT).decode('utf-8')
     lines = work.split('\n')
-    print(len(lines), 'lines')
     for line in lines:
+        if 'Linux' in line:
+            parts = line.split()
+            datestamp = parts[3]
+            # take the base system name, delete the '(':
+            sysname   = parts[2].split('.')[0].strip('(')
+            continue
+
         if 'CPU' in line or 'all' in line or len(line) == 0 or 'Average' in line:
             continue
 
         parts = line.split()
-        # 00:00:00
-        h, m, s = parts[0].split(':')
-        hour    = int(h)
-        minute  = int(m)
-        seconds = int(s)
+        # do a list comprehension:
+        hour, minute, seconds = [int(x) for x in parts[0].split(':')]
+
         if parts[1] == 'AM' and hour == 12:
             hour = 0
+
         if parts[1] == 'PM' and hour != 12:
             hour += 12
 
         timestr = str(hour).zfill(2) + ':' + str(minute).zfill(2) + ':' + str(seconds).zfill(2)
-        idle = float(parts[8]);
-        if idle < minusage:
-           print(timestr, 'idle:', parts[8] + '%')
+        idle = 100 - float(parts[8]);
+        if idle > maxusage:
+           print(datestamp, timestr, sysname, 'CPU', parts[2], 'usage:', str(round(idle, 2)) + '%')
 
 
         continue
